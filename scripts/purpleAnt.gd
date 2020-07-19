d3f7a81884b34f7bcb95716980271785b4023d22
AdmiralOct <buildgamesnotbombs@gmail.com>
2020-07-19 17:09:33 -0400

    Audio and SFX added

--- scripts/purpleAnt.gd
+++ scripts/purpleAnt.gd
@@ -10,18 +10,30 @@ var newArr = []
 
 onready var sugar_scene = preload("res://scenes/sugar.tscn")
 
+onready var footstepSound = preload("res://Sounds/footstep.wav")
+onready var JumpSugarSound = preload("res://Sounds/jump-8.wav")
+onready var placeSugarSound = preload("res://Sounds/hit-4.wav")
+
+
+
 func _physics_process(delta):
+	
+	if Input.is_action_just_pressed("ui_right") or Input.is_action_just_pressed("ui_left"):
+		$AudioStreamPlayer2D.set_stream(footstepSound)
+		$AudioStreamPlayer2D.play()
 	motion.y += GRAVITY
 	if Input.is_action_pressed("ui_right"):
 		$AnimationPlayer.play("walk")
 		$Sprite.flip_h = false
 		$sugarSpawner.position.x = 48
 		motion.x = min(motion.x + ACCELERATION, SPEED)
+		
 	elif Input.is_action_pressed("ui_left"):
 		$AnimationPlayer.play("walk")
 		$Sprite.flip_h = true
 		$sugarSpawner.position.x = -48
 		motion.x = max(motion.x - ACCELERATION, -SPEED)
+		
 	else:
 		$AnimationPlayer.play("idle")
 		motion.x = 0
@@ -46,6 +58,8 @@ func _process(delta):
 			print(newArr[0].get_parent())
 			self.position.x = newArr[0].global_position.x
 			position.y = newArr[0].global_position.y-48
+			$AudioStreamPlayer2D.set_stream(JumpSugarSound)
+			$AudioStreamPlayer2D.play()
 		else:
 			var sugar = sugar_scene.instance()
 			sugar.position = $sugarSpawner.global_position
@@ -54,6 +68,8 @@ func _process(delta):
 			canShoot = false
 			yield(get_tree().create_timer(0.3), "timeout")
 			canShoot = true
+			$AudioStreamPlayer2D.set_stream(placeSugarSound)
+			$AudioStreamPlayer2D.play(0.05)
 			
 
 
